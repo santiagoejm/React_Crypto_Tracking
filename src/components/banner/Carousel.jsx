84d01ce74be4d8a81e60dd3b9c1bdc565d8917e2
Santiago Julian <sjulian983@gmail.com>
2022-03-03 19:20:40 +0100

    carousel initiazed

--- src/components/banner/Carousel.jsx
+++ src/components/banner/Carousel.jsx
@@ -1,8 +1,9 @@
-import { makeStyles } from "@material-ui/core";
+import { Link, makeStyles } from "@material-ui/core";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { CryptoState } from "../../context/ContextCrypto";
 import { TrendingCoins } from "../../config/api";
+import AliceCarousel from "react-alice-carousel";
 
 const useStyles = makeStyles((theme) => ({
   carousel: {
@@ -27,9 +28,47 @@ const Carousel = () => {
     fetchTrendingCoins();
   }, [currency]);
 
-  console.log(trending);
+  const items = trending.map((coin) => {
+    let profit = coin.price_change_percentage_24h >= 0;
+    return (
+      <Link to={`/coins/${coin.id}`} className={classes.carouselItem}>
+        <img src={coin?.image} alt={coin.name} height="80" />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span>
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+      </Link>
+    );
+  });
 
-  return <div className={classes.carousel}>Carousel</div>;
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
+
+  return (
+    <div className={classes.carousel}>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        autoPlay
+        items={items}
+      />
+    </div>
+  );
 };
 
 export default Carousel;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import ContextCrypto from "./context/ContextCrypto";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 ReactDOM.render(
   <React.StrictMode>
